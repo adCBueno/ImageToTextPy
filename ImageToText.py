5c1a3e9df3fb634a68836f3141dcdea708b203ad
AdriCBueno <58597644+adCBueno@users.noreply.github.com>
2022-04-29 21:07:53 -0400

    Add files via upload

---
+++ ImageToText.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+get_ipython().system('pip install tesseract')
+
+
+# In[2]:
+
+
+get_ipython().system('pip3 install --upgrade Pillow')
+
+
+# In[3]:
+
+
+get_ipython().system('pip install Pillow==9.1.0')
+
+
+# In[4]:
+
+
+get_ipython().system('pip install pytesseract')
+
+
+# uploaded = files.upload()![imagen.jpg](attachment:imagen.jpg)
+
+# ![imagen.jpg](attachment:imagen.jpg)
+
+# In[30]:
+
+
+# import image module
+from IPython.display import Image
+
+# get the image
+Image(url="imagen.jpg", width=300, height=300)
+
+
+# In[31]:
+
+
+import pytesseract
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+#%matplotlib inline
+
+
+# In[32]:
+
+
+texto_largo = plt.imread('imagen.jpg')
+plt.figure(figsize=(15,5))
+plt.imshow(texto_largo)
+plt.axis(False)
+
+
+# In[33]:
+
+
+img = plt.imread('imagen.jpg')
+
+
+# In[34]:
+
+
+plt.imshow(img)
+
+
+# In[41]:
+
+
+get_ipython().system('pip install tesseract')
+get_ipython().system('pip install pytesseract')
+
+
+# In[57]:
+
+
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract' #Download it on your PC and put the path, it might change
+print(pytesseract.image_to_string(r'imagen.jpg')) #With the name of your image
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
